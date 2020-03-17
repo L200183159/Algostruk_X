18747fb65bd690cff8756be4ee2c5020f9ea7d74
Naura Salsabila <44856077+L200183159@users.noreply.github.com>
2020-03-17 20:11:50 +0700

    Modul 3

--- 2.py
+++ 2.py
@@ -1,27 +1,35 @@
-class Mahasiswa():
-    def __init__(self, nama, NIM, kota, us):
-        self.nama = nama
-        self.NIM = NIM
-        self.kotaTinggal = kota
-        self.uangSaku = us
-    def __str__(self):
-        s = self.nama+ ", NIM " + str(self.NIM) \
-            + ". Tinggal di " + self.kotaTinggal \
-            + ". Uangsaku Rp " + str(self.uangSaku) \
-            + " tiap bulannya."
-        return s
-    def ambilNama(self):
-        return self.nama
-    def ambilNIM(self):
-        return self.NIM
-    def ambilUangSaku(self):
-        return self.uangSaku
-    def makan(self, s):
-        print("Saya baru saja makan", s, "sambil belajar.")
-        self.keadaan = "kenyang"
-    def ambilKotaTinggal(self):
-        return self.kotaTinggal
-    def perbaruiKotaTinggal(self, kotaBaru):
-        self.kotaTinggal = kotaBaru
-    def tambahUangSaku(self, tambahUang):
-        self.uangSaku += tambahUang
+def buatNol(a,b = None):
+    if b == None:
+        c = []
+        e = 0
+        while len(c) != a:
+            d = []
+            while len(d) != a:
+                d.append(e)
+            c.append(d)
+        print(c)
+    else:
+        c = []
+        e = 0
+        while len(c) != a:
+            d = []
+            while len(d) != b:
+                d.append(e)
+            c.append(d)
+        print(c)
+            
+def buatIdentitas(f):
+    g = []
+    h = 0
+    i = 1
+    j = 0
+    while len(g) != f:
+        k = []
+        while len(k) != f:
+            if len(k) == j:
+                k.append(i)
+            else:
+                k.append(h)
+        g.append(k)
+        j += 1
+    print(g)
