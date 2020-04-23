262f73a11a130890a4710d5e9bee287e33233119
Naura Salsabila <44856077+L200183159@users.noreply.github.com>
2020-04-23 12:18:50 +0700

    mod7

--- 2.py
+++ 2.py
@@ -1,16 +1,7 @@
-x = [1,3,5,7,9]
-y = [2,4,6,8,10]
-z = x + y
+import re
 
-v = 0
-for j in range(len(x)):
-    w = z[v]
-    for i in z[v:]:
-        if w > i:
-            w = i
-        else:
-            continue
-    z.remove(w)
-    z.insert(v, w)
-    v += 1
-print(z)
+f = open("Indonesia.txt", "r", encoding="latin1")
+teks = f.read()
+f.close()
+string = re.findall(r"di\w+", teks)
+print(string)
--- 4.py
+++ 4.py
@@ -1,29 +1,17 @@
-class MhsTIF:
-    def __init__(self, nama, nim, kota, uang):
-        self.nama = nama
-        self.nim = nim
-        self.kota = kota
-        self.uang = uang
+import re
 
-c0 = MhsTIF("Ika", 10, "Sukoharjo", 240000)
-c1 = MhsTIF("Budi", 51, "Sragen", 230000)
-c2 = MhsTIF("Ahmad", 2, "Surakarta", 250000)
-c3 = MhsTIF("Chandra", 18, "Surakarta", 235000)
-c4 = MhsTIF("Eka", 4, "Boyolali", 240000)
-c5 = MhsTIF("Fandi", 31, "Salatiga", 250000)
-c6 = MhsTIF("Deni", 13, "Klaten", 245000)
-c7 = MhsTIF("Galuh", 5, "Wonogiri", 245000)
-c8 = MhsTIF("Janto", 23, "Klaten", 245000)
-c9 = MhsTIF("Hasan", 64, "Karanganyar", 270000)
-c10 = MhsTIF("Khalid", 29, "Purwodadi", 265000)
+f = open("KEI.html", "r", encoding="latin1")
+teks = f.read()
+f.close()
 
-Daftar = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
+pola =  r'(\w+)</a></td>\
+<td>\d\.\d\d</td>\
+<td>\d\.\d\d</td>\
+<td>\d\.\d\d</td>\
+<td>(\d.\d\d)'
 
-a = 250000
-b = []
-for i in Daftar:
-    if i.uang < a:
-        b.append(i.nama)
-    else:
-        continue
-print(b)
+data = re.findall(pola, teks)
+isi = []
+for i in data:
+    isi.append((i[0], float(i[1])))
+print(isi)
